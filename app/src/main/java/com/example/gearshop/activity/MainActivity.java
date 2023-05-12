fcbd6ebf56a30ffa3c475a0e6f9a9734b7419e35
Surgeous <tuanbao2311@gmail.com>
2023-05-12 18:48:16 +0700

    Tạo Activity Class và Database Class

---
+++ app/src/main/java/com/example/gearshop/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gearshop.activity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gearshop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
