6d83e7120ac7a2ff3a5993a3c7283447dd471542
Surgeous <tuanbao2311@gmail.com>
2023-05-12 18:55:43 +0700

    Tạo lại AzureSQLDatabase kết nối với database trên cloud.

--- app/src/main/java/com/example/gearshop/database/AzureSQLDatabase.java
+++ app/src/main/java/com/example/gearshop/database/AzureSQLDatabase.java
@@ -1,4 +1,42 @@
 package com.example.gearshop.database;
 
-public class AzureSQLDatabase {
+import android.os.AsyncTask;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class AzureSQLDatabase extends AsyncTask<String, Void, ResultSet> {
+    final String AzureConnectionString =
+            "jdbc:jtds:sqlserver://dozlap.database.windows.net:1433;" +
+            "databaseName=dozlap;" +
+            "user=sqladmin@dozlap;" +
+            "password=CodingProject123@;" +
+            "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;" +
+            "loginTimeout=30;ssl=TLSv1.2";
+    @Override
+    protected ResultSet doInBackground(String... sqlCommand) {
+        ResultSet resultSet = null;
+        try {
+            Connection connection = DriverManager.getConnection(AzureConnectionString);
+            Statement statement = connection.createStatement();
+            if (sqlCommand[0].contains("SELECT")){
+                resultSet = statement.executeQuery(sqlCommand[0]);
+            }
+            else{
+                statement.execute(sqlCommand[0]);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+    @Override
+    protected void onPostExecute(ResultSet result) {
+        // do something
+    }
 }
