44c8cf3ba899e67b2088884d4d19c7aa1bdfb0e1
Nguyen Kien <trungkien2003ntk@gmail.com>
2023-05-14 18:13:26 +0700

    Update MainActivity.java
    Add some code for getting Logged in userId to use in this home page

--- app/src/main/java/com/example/gearshop/activity/MainActivity.java
+++ app/src/main/java/com/example/gearshop/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.gearshop.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.gearshop.R;
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String userId = prefs.getString("customerId", null);
+
+
+        TextView titleScreen = (TextView) findViewById(R.id.title_screen);
+
+        titleScreen.setText(userId);
     }
 }
