84881272df1935f39b8d1bb9aa615b71ab1c0027
SurgeousJP <tuanbao2311@gmail.com>
2023-07-03 17:10:57 +0700

    tạo button load danh sách sản phẩm

--- app/src/main/java/com/example/gearshop/activity/admin_activity/AdminProductCategoryManagementActivity.java
+++ app/src/main/java/com/example/gearshop/activity/admin_activity/AdminProductCategoryManagementActivity.java
@@ -3,9 +3,11 @@ package com.example.gearshop.activity.admin_activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.gearshop.R;
 import com.example.gearshop.adapter.CategoryListViewAdapter;
@@ -13,6 +15,10 @@ import com.example.gearshop.model.Category;
 import com.example.gearshop.repository.CategoryRepository;
 import com.example.gearshop.utility.ActivityStartManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class AdminProductCategoryManagementActivity extends AppCompatActivity {
@@ -20,6 +26,9 @@ public class AdminProductCategoryManagementActivity extends AppCompatActivity {
     private View CustomerManagementView;
     private View OrderManagementView;
     private View AccountManagementView;
+    private TextView LoadProductListTextView;
+    private static final int REQUEST_CODE_FILE_PICKER = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +51,11 @@ public class AdminProductCategoryManagementActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        LoadProductListTextView = findViewById(R.id.load_multiple_products_text);
+        LoadProductListTextView.setOnClickListener(view -> {
+            openCSVFilePicker();
+        });
+
         CustomerManagementView = findViewById(R.id.product_customer_management);
         CustomerManagementView.setOnClickListener(view -> {
             ActivityStartManager.startTargetActivity(getBaseContext(), AdminCustomerManagementActivity.class);
@@ -55,4 +69,45 @@ public class AdminProductCategoryManagementActivity extends AppCompatActivity {
 
         });
     }
+    private void openCSVFilePicker() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("csv"); // Specify the MIME type to restrict file selection to CSV files
+
+        startActivityForResult(intent, REQUEST_CODE_FILE_PICKER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_FILE_PICKER && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri uri = data.getData();
+                if (uri != null) {
+                    // Process the selected CSV file
+                    readCsvFile(uri);
+                }
+            }
+        }
+    }
+
+    private void readCsvFile(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Process each line of the CSV file
+                String[] values = line.split(",");
+                // Access the values array to get individual column values
+                // ...
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
