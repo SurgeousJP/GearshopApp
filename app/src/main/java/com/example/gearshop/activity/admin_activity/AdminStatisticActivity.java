9dfed0c166055350a0b5bf07eddf3937f3181795
SurgeousJP <tuanbao2311@gmail.com>
2023-07-05 11:40:01 +0700

    chỉnh sửa đôi chút cho admin info và statistic

--- app/src/main/java/com/example/gearshop/activity/admin_activity/AdminStatisticActivity.java
+++ app/src/main/java/com/example/gearshop/activity/admin_activity/AdminStatisticActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.gearshop.R;
+
 public class AdminStatisticActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_statistic);
     }
 }
