eb6f8eb1bddcb6e28694cb8644e721cfba37f023
SurgeousJP <tuanbao2311@gmail.com>
2023-07-05 11:33:19 +0700

    Hoàn thành AdminOtherActivity

---
+++ app/src/main/java/com/example/gearshop/activity/admin_activity/AdminInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.gearshop.activity.admin_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
